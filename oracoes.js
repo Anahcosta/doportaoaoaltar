77f31a327f9f17d6fd5a1d802e9dccd9b3d41b74
Anahcosta <costaganapaula@gmail.com>
2026-07-31 16:34:27 -0300

    Update oracoes.js

--- oracoes.js
+++ oracoes.js
@@ -1,32 +1,51 @@
 const oracoes = [
-  {titulo:"Oração ao Espírito Santo",texto:`Vinde, Espírito Santo, enchei os corações dos vossos fiéis e acendei neles o fogo do vosso amor.
+  {
+    titulo: "Oração ao Espírito Santo",
+    texto: `Vinde, Espírito Santo, enchei os corações dos vossos fiéis e acendei neles o fogo do vosso amor.
 
 Enviai o vosso Espírito e tudo será criado. E renovareis a face da terra.
 
-Oremos: Ó Deus, que instruístes os corações dos vossos fiéis com a luz do Espírito Santo, fazei que apreciemos retamente todas as coisas segundo o mesmo Espírito e gozemos sempre da sua consolação. Por Cristo, Senhor nosso. Amém.`},
-  {titulo:"Oferecimento do Terço",texto:`Divino Jesus, nós Vos oferecemos este terço que vamos rezar, contemplando os mistérios da nossa redenção.
+Oremos: Ó Deus, que instruístes os corações dos vossos fiéis com a luz do Espírito Santo, fazei que apreciemos retamente todas as coisas segundo o mesmo Espírito e gozemos sempre da sua consolação. Por Cristo, Senhor nosso. Amém.`
+  },
+  {
+    titulo: "Oferecimento do Terço",
+    texto: `Divino Jesus, nós Vos oferecemos este terço que vamos rezar, contemplando os mistérios da nossa redenção.
 
-Concedei-nos, pela intercessão da Virgem Maria, Mãe de Deus e nossa Mãe, as virtudes necessárias para bem rezá-lo e a graça de vivermos os ensinamentos nele contidos.`},
-  {titulo:"Mistérios Dolorosos",imagem:"misterios-dolorosos.jpg",texto:`1. A agonia de Jesus no Horto das Oliveiras.
+Concedei-nos, pela intercessão da Virgem Maria, Mãe de Deus e nossa Mãe, as virtudes necessárias para bem rezá-lo e a graça de vivermos os ensinamentos nele contidos.`
+  },
+  {
+    titulo: "Mistérios Dolorosos",
+    imagem: "misterios-dolorosos.jpg",
+    texto: `1. A agonia de Jesus no Horto das Oliveiras.
 2. A flagelação de Jesus.
 3. A coroação de espinhos.
 4. Jesus carrega a cruz.
-5. A crucificação e morte de Jesus.`},
-  {titulo:"Jaculatória",texto:`Ó meu Jesus, perdoai-nos, livrai-nos do fogo do inferno, levai as almas todas para o Céu e socorrei principalmente as que mais precisarem.`},
-  {titulo:"Agradecimento",texto:`Infinitas graças vos damos, soberana Rainha, pelos benefícios que todos os dias recebemos de vossas mãos liberais.
+5. A crucificação e morte de Jesus.`
+  },
+  {
+    titulo: "Jaculatória",
+    texto: `Ó meu Jesus, perdoai-nos, livrai-nos do fogo do inferno, levai as almas todas para o Céu e socorrei principalmente as que mais precisarem.`
+  },
+  {
+    titulo: "Agradecimento",
+    texto: `Infinitas graças vos damos, soberana Rainha, pelos benefícios que todos os dias recebemos de vossas mãos liberais.`
+  },
+  {
+    titulo: "Salve-Rainha",
+    texto: `Salve, Rainha, Mãe de misericórdia, vida, doçura e esperança nossa, salve!
 
-Dignai-vos, agora e para sempre, tomar-nos debaixo do vosso poderoso amparo e, para mais vos obrigar, vos saudamos com uma Salve-Rainha.`},
-  {titulo:"Salve-Rainha",texto:`Salve, Rainha, Mãe de misericórdia, vida, doçura e esperança nossa, salve!
+A vós bradamos, os degredados filhos de Eva.
+A vós suspiramos, gemendo e chorando neste vale de lágrimas.
 
-A vós bradamos, os degredados filhos de Eva. A vós suspiramos, gemendo e chorando neste vale de lágrimas.
-
-Eia, pois, advogada nossa, esses vossos olhos misericordiosos a nós volvei. E, depois deste desterro, mostrai-nos Jesus, bendito fruto do vosso ventre.
+Eia, pois, advogada nossa, esses vossos olhos misericordiosos a nós volvei.
+E, depois deste desterro, mostrai-nos Jesus, bendito fruto do vosso ventre.
 
 Ó clemente, ó piedosa, ó doce sempre Virgem Maria.
 
-Rogai por nós, santa Mãe de Deus, para que sejamos dignos das promessas de Cristo. Amém.`}
-{
-  titulo: "Oração de Súplica pelo Futuro do Grupo",
-  imagem: "oracao-futuro-grupo.jpg.jpeg"
-}
+Rogai por nós, santa Mãe de Deus, para que sejamos dignos das promessas de Cristo. Amém.`
+  },
+  {
+    titulo: "Oração de Súplica pelo Futuro do Grupo",
+    imagem: "oracao-futuro-grupo.jpg.jpeg"
+  }
 ];
