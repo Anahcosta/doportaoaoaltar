fc7848d252b74c11e60b3d0894917a3aa7416f15
Anahcosta <costaganapaula@gmail.com>
2026-07-31 15:21:06 -0300

    Update musicas.js

--- musicas.js
+++ musicas.js
@@ -1,5 +1,139 @@
 const musicas = [
-  {titulo:"Música de abertura",letra:`Cole aqui a letra da música escolhida.`},
-  {titulo:"Música durante o encontro",letra:`Cole aqui a letra da música escolhida.`},
-  {titulo:"Música de encerramento",letra:`Cole aqui a letra da música escolhida.`}
+  {
+    titulo: "01. O Senhor Tem Muitos Filhos (acolhida)",
+    letra: `
+    O Senhor tem muitos filhos 
+    Muitos filhos ele tem 
+    Eu sou um deles 
+    Vocês também 
+    Louvemos ao senhor 
+    Braço direito 
+    Braço esquerdo 
+    Perna direita 
+    Perna esquerda
+    `
+  },
+  {
+    titulo: "02. Em Nome do Pai (início da oração)",
+    letra: `
+    Em nome do Pai 
+    Em nome do Filho 
+    Em nome do Espírito Santo, 
+    estamos aqui
+    
+    Para louvar e agradecer, 
+    bendizer e adorar, 
+    estamos aqui, Senhor, 
+    a teu dispor 
+    
+    Para louvar e agradecer, 
+    bendizer e adorar, 
+    te aclamar, Deus trino de amor
+    `
+  },
+  {
+    titulo: "03. Confia em Mim (meditação)",
+    letra: `
+    Vem, que a tempestade já não pode te abalar 
+    A segurança em meu barco encontrarás 
+    Confia em mim, o meu amor te abrigará 
+    Sei que, angustiado, o coração se endureceu 
+    Mas eu entendo tudo o que te aconteceu 
+    Ainda é tempo de voltar para o teu Deus 
+    
+    Não tenhas medo, pois eu estou aqui 
+    É o teu Senhor quem diz 
+    Quero guiar os passos teus 
+    Vem, entrega-te, então 
+    Farei morada em teu coração 
+    E quando anoitecer, cansado eu te encontrar 
+    No silêncio teu eu irei te consolar 
+    Nos braços meus, descansarás 
+    
+    Forças te darei Forças te darei 
+    `
+  },
+  {
+    titulo: "04. Renova-me",
+    letra: `
+    Renova-me, Senhor Jesus 
+    Já não quero ser igual 
+    Renova-me, Senhor Jesus 
+    Põe em mim Teu coração 
+    
+    Porque tudo que há dentro de mim 
+    Precisa ser mudado, Senhor 
+    Porque tudo que há dentro do meu coração 
+    Precisa mais de Ti
+    `
+  },
+  {
+    titulo: "05. Ninguém Te Ama Como Eu",
+    letra: `
+    Tenho esperado este momento 
+    Tenho esperado que viesses a mim 
+    Tenho esperado que me fales 
+    Tenho esperado que estivesses assim
+    
+    Eu sei bem que tens vivido 
+    Sei também que tens chorado 
+    Eu sei bem que tens sofrido 
+    Pois permaneço ao teu lado
+    
+    Ninguém te ama como eu 
+    Ninguém te ama como eu 
+    Olhe pra cruz Esta é a minha grande prova 
+    Ninguém te ama como eu 
+    
+    Eu sei bem o que me dizes 
+    Ainda que nunca me fales 
+    Eu sei bem o que tens sentido 
+    Ainda que nunca me reveles 
+    
+    Tenho andado ao teu lado 
+    Junto a ti permanecido 
+    Eu te levo em meus braços 
+    Pois sou teu melhor amigo
+    `
+  },
+  {
+    titulo: "06. Regaço Acolhedor",
+    letra: `
+    Óh minh'alma 
+    Retorna tua paz 
+    Como criança bem tranquila 
+    No Regaço acolhedor de sua mãe 
+    
+    Óh minh'alma 
+    Retorna tua paz 
+    Como criança bem tranquila 
+    No Regaço acolhedor de sua mãe 
+    
+    Minha mãe é a Virgem Maria 
+    É ela que agora vai me acolher 
+    Me abraçar, me perdoar 
+    Me compreender 
+    Me acalmar, me ensinar 
+    Me educar Me formar, me amar
+    `
+  },
+  {
+    titulo: "07. Aperte a Minha Mão (encerramento)",
+    letra: `
+    Aperte a minha mão, irmão, 
+    quero te dar a paz 
+    Do meu senhor Me dê um abraço forte, 
+    bem mais forte, muito forte! 
+    Num gesto de amor 
+    
+    A paz do senhor 
+    Quero te dar, meu irmão! 
+    Com toda alegria que existe 
+    No meu coração 
+    A paz do senhor 
+    Quero te dar, meu irmão! 
+    Com toda alegria que existe 
+    No meu coração
+    `
+  }
 ];
